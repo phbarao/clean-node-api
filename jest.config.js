2b85a32a10af0764112e271a60e03d1eb170f813
Pedro Barão <phbarao@gmail.com>
2022-08-27 16:25:01 -0300

    chore: add coveragePathIgnorePatterns to jest.config file

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts',
+    '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '<rootDir>/src/presentation/protocols/index.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
